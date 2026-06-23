c31ac794d8269e461141e7b5d846747ceed6dfca
Cristiano Ribeiro <cristianoribeiromed@gmail.com>
2026-06-23 11:44:53 +0000

    Adiciona pagamento via PIX (QR + copia-e-cola) gerado dinamicamente; precos em R$; combo por materia

--- config.js
+++ config.js
@@ -2,6 +2,9 @@
 const CONFIG = {
   whatsappNumero: "5547991572214", // DDI 55 (Brasil) + DDD 47 + número. Troque aqui se mudar.
   nomeMarca: "Resumos Clinicus",
+  pixChave: "cristianoribeiromed@gmail.com", // chave PIX (email)
+  pixNome: "Cristiano Ribeiro da Silva",       // precisa bater com o nome no banco
+  pixCidade: "Blumenau",                       // cidade da conta bancária
   mensagemWhatsappTemplate: (nome, item, preco) =>
     `Olá! Acabei de pagar *${item}*${preco ? " (Gs. " + preco + ")" : ""}. Meu nome é ${nome || "___"} e meu email é ___. Segue o comprovante:`
 };
